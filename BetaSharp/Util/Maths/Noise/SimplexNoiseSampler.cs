tomast1337/omniblock

Add more names, plus some small code cleanups (#108)
* low-hanging fruit

* Name all of MinecraftServer

* more names, constants, etc.

* Ease merging

* Safe refactors

* More names

* Names for simplex noise, mostly

* Name those perlin noise variables like my life depends on it

* name mapitemrenderer

* Fix whatever was very wrong with GuiStats

* Improve color constants and name some stuff

* Replace System.out.println with Console.WriteLine

* Waste time by adding names to an unused class

* Carver names

* Use collection expressions

* Unwrap 'params' arguments

* Fix merge

* Fix color constants

BetaSharp/Util/Maths/Noise/SimplexNoiseSampler.cs
@@ -3,10 +3,10 @@ namespace BetaSharp.Util.Maths.Noise;
 public class SimplexNoiseSampler : java.lang.Object
 {
     private static readonly int[][] grads = [[1, 1, 0], [-1, 1, 0], [1, -1, 0], [-1, -1, 0], [1, 0, 1], [-1, 0, 1], [1, 0, -1], [-1, 0, -1], [0, 1, 1], [0, -1, 1], [0, 1, -1], [0, -1, -1]];
-    private readonly int[] perm;
-    public double offsetX;
-    public double offsetY;
-    public double offsetZ;
+    private readonly int[] _permutations;
+    private readonly double _xCoord;
+    private readonly double _yCoord;
+    private readonly double _zCoord;
     private static readonly double F2 = 0.5D * (java.lang.Math.sqrt(3.0D) - 1.0D);
     private static readonly double G2 = (3.0D - java.lang.Math.sqrt(3.0D)) / 6.0D;
 
@@ -14,118 +14,117 @@ public class SimplexNoiseSampler : java.lang.Object
     {
     }
 
-    public SimplexNoiseSampler(java.util.Random var1)
+    public SimplexNoiseSampler(java.util.Random rand)
     {
-        perm = new int[512];
-        offsetX = var1.nextDouble() * 256.0D;
-        offsetY = var1.nextDouble() * 256.0D;
-        offsetZ = var1.nextDouble() * 256.0D;
+        _permutations = new int[512];
+        _xCoord = rand.nextDouble() * 256.0D;
+        _yCoord = rand.nextDouble() * 256.0D;
+        _zCoord = rand.nextDouble() * 256.0D;
 
-        int var2;
-        for (var2 = 0; var2 < 256; perm[var2] = var2++)
+        // Fill perm with values from 0 to 255 in random order, duplicating the first 256 values to the end of the array
+        for (int i = 0; i < 256; i++)
         {
+            _permutations[i] = i;
         }
 
-        for (var2 = 0; var2 < 256; ++var2)
+        for (int i = 0; i < 256; ++i)
         {
-            int var3 = var1.nextInt(256 - var2) + var2;
-            (perm[var2], perm[var3]) = (perm[var3], perm[var2]);
-            perm[var2 + 256] = perm[var2];
+            int j = rand.nextInt(256 - i) + i;
+            (_permutations[i], _permutations[j]) = (_permutations[j], _permutations[i]);
+            _permutations[i + 256] = _permutations[i];
         }
 
     }
 
-    private static int wrap(double var0)
+    private static int floor(double num)
     {
-        return var0 > 0.0D ? (int)var0 : (int)var0 - 1;
+        return num > 0.0D ? (int)num : (int)num - 1;
     }
 
-    private static double func_4156_a(int[] var0, double var1, double var3)
+    private static double dot(int[] gradient, double dx, double dy)
     {
-        return var0[0] * var1 + var0[1] * var3;
+        return gradient[0] * dx + gradient[1] * dy;
     }
 
-    public void func_4157_a(double[] var1, double var2, double var4, int var6, int var7, double var8, double var10, double var12)
+    public void sample(double[] buffer, double x, double z, int width, int depth, double xFrequency, double zFrequency, double amplitude)
     {
-        int var14 = 0;
+        int counter = 0;
 
-        for (int var15 = 0; var15 < var6; ++var15)
+        for (int x1 = 0; x1 < width; ++x1)
         {
-            double var16 = (var2 + var15) * var8 + offsetX;
+            double x2 = (x + x1) * xFrequency + _xCoord;
 
-            for (int var18 = 0; var18 < var7; ++var18)
+            for (int z1 = 0; z1 < depth; ++z1)
             {
-                double var19 = (var4 + var18) * var10 + offsetY;
-                double var27 = (var16 + var19) * F2;
-                int var29 = wrap(var16 + var27);
-                int var30 = wrap(var19 + var27);
-                double var31 = (var29 + var30) * G2;
-                double var33 = var29 - var31;
-                double var35 = var30 - var31;
-                double var37 = var16 - var33;
-                double var39 = var19 - var35;
-                byte var41;
-                byte var42;
-                if (var37 > var39)
+                double z2 = (z + z1) * zFrequency + _yCoord;
+                double s = (x2 + z2) * F2;
+                int i = floor(x2 + s);
+                int j = floor(z2 + s);
+                double t = (i + j) * G2;
+                double x3 = i - t;
+                double z3 = j - t;
+                double x4 = x2 - x3;
+                double z4 = z2 - z3;
+                byte i1;
+                byte j1;
+                if (x4 > z4)
                 {
-                    var41 = 1;
-                    var42 = 0;
+                    i1 = 1;
+                    j1 = 0;
                 }
                 else
                 {
-                    var41 = 0;
-                    var42 = 1;
+                    i1 = 0;
+                    j1 = 1;
                 }
 
-                double var43 = var37 - var41 + G2;
-                double var45 = var39 - var42 + G2;
-                double var47 = var37 - 1.0D + 2.0D * G2;
-                double var49 = var39 - 1.0D + 2.0D * G2;
-                int var51 = var29 & 255;
-                int var52 = var30 & 255;
-                int var53 = perm[var51 + perm[var52]] % 12;
-                int var54 = perm[var51 + var41 + perm[var52 + var42]] % 12;
-                int var55 = perm[var51 + 1 + perm[var52 + 1]] % 12;
-                double var56 = 0.5D - var37 * var37 - var39 * var39;
-                double var21;
-                if (var56 < 0.0D)
+                double x5 = x4 - i1 + G2;
+                double z5 = z4 - j1 + G2;
+                double x6 = x4 - 1.0D + 2.0D * G2;
+                double z6 = z4 - 1.0D + 2.0D * G2;
+                int ii = i & 255;
+                int jj = j & 255;
+                int gi0 = _permutations[ii + _permutations[jj]] % 12;
+                int gi1 = _permutations[ii + i1 + _permutations[jj + j1]] % 12;
+                int gi2 = _permutations[ii + 1 + _permutations[jj + 1]] % 12;
+                double t0 = 0.5D - x4 * x4 - z4 * z4;
+                double n0;
+                if (t0 < 0.0D)
                 {
-                    var21 = 0.0D;
+                    n0 = 0.0D;
                 }
                 else
                 {
-                    var56 *= var56;
-                    var21 = var56 * var56 * func_4156_a(grads[var53], var37, var39);
+                    t0 *= t0;
+                    n0 = t0 * t0 * dot(grads[gi0], x4, z4);
                 }
 
-                double var58 = 0.5D - var43 * var43 - var45 * var45;
-                double var23;
-                if (var58 < 0.0D)
+                double t1 = 0.5D - x5 * x5 - z5 * z5;
+                double n1;
+                if (t1 < 0.0D)
                 {
-                    var23 = 0.0D;
+                    n1 = 0.0D;
                 }
                 else
                 {
-                    var58 *= var58;
-                    var23 = var58 * var58 * func_4156_a(grads[var54], var43, var45);
+                    t1 *= t1;
+                    n1 = t1 * t1 * dot(grads[gi1], x5, z5);
                 }
 
-                double var60 = 0.5D - var47 * var47 - var49 * var49;
-                double var25;
-                if (var60 < 0.0D)
+                double t2 = 0.5D - x6 * x6 - z6 * z6;
+                double n2;
+                if (t2 < 0.0D)
                 {
-                    var25 = 0.0D;
+                    n2 = 0.0D;
                 }
                 else
                 {
-                    var60 *= var60;
-                    var25 = var60 * var60 * func_4156_a(grads[var55], var47, var49);
+                    t2 *= t2;
+                    n2 = t2 * t2 * dot(grads[gi2], x6, z6);
                 }
 
-                int var10001 = var14++;
-                var1[var10001] += 70.0D * (var21 + var23 + var25) * var12;
+                buffer[counter++] += 70.0D * (n0 + n1 + n2) * amplitude;
             }
         }
-
     }
 }

Fazin85/betasharp

BetaSharp/Util/Maths/Noise/SimplexNoiseSampler.cs
@@ -3,10 +3,10 @@ namespace BetaSharp.Util.Maths.Noise;
 public class SimplexNoiseSampler : java.lang.Object
 {
     private static readonly int[][] grads = [[1, 1, 0], [-1, 1, 0], [1, -1, 0], [-1, -1, 0], [1, 0, 1], [-1, 0, 1], [1, 0, -1], [-1, 0, -1], [0, 1, 1], [0, -1, 1], [0, 1, -1], [0, -1, -1]];
-    private readonly int[] perm;
-    public double offsetX;
-    public double offsetY;
-    public double offsetZ;
+    private readonly int[] _permutations;
+    private readonly double _xCoord;
+    private readonly double _yCoord;
+    private readonly double _zCoord;
     private static readonly double F2 = 0.5D * (java.lang.Math.sqrt(3.0D) - 1.0D);
     private static readonly double G2 = (3.0D - java.lang.Math.sqrt(3.0D)) / 6.0D;
 
@@ -14,118 +14,117 @@ public class SimplexNoiseSampler : java.lang.Object
     {
     }
 
-    public SimplexNoiseSampler(JavaRandom var1)
+    public SimplexNoiseSampler(JavaRandom rand)
     {
-        perm = new int[512];
-        offsetX = var1.NextDouble() * 256.0D;
-        offsetY = var1.NextDouble() * 256.0D;
-        offsetZ = var1.NextDouble() * 256.0D;
+        _permutations = new int[512];
+        _xCoord = rand.NextDouble() * 256.0D;
+        _yCoord = rand.NextDouble() * 256.0D;
+        _zCoord = rand.NextDouble() * 256.0D;
 
-        int var2;
-        for (var2 = 0; var2 < 256; perm[var2] = var2++)
+        // Fill perm with values from 0 to 255 in random order, duplicating the first 256 values to the end of the array
+        for (int i = 0; i < 256; i++)
         {
+            _permutations[i] = i;
         }
 
-        for (var2 = 0; var2 < 256; ++var2)
+        for (int i = 0; i < 256; ++i)
         {
-            int var3 = var1.NextInt(256 - var2) + var2;
-            (perm[var2], perm[var3]) = (perm[var3], perm[var2]);
-            perm[var2 + 256] = perm[var2];
+            int j = rand.NextInt(256 - i) + i;
+            (_permutations[i], _permutations[j]) = (_permutations[j], _permutations[i]);
+            _permutations[i + 256] = _permutations[i];
         }
 
     }
 
-    private static int wrap(double var0)
+    private static int floor(double num)
     {
-        return var0 > 0.0D ? (int)var0 : (int)var0 - 1;
+        return num > 0.0D ? (int)num : (int)num - 1;
     }
 
-    private static double func_4156_a(int[] var0, double var1, double var3)
+    private static double dot(int[] gradient, double dx, double dy)
     {
-        return var0[0] * var1 + var0[1] * var3;
+        return gradient[0] * dx + gradient[1] * dy;
     }
 
-    public void func_4157_a(double[] var1, double var2, double var4, int var6, int var7, double var8, double var10, double var12)
+    public void sample(double[] buffer, double x, double z, int width, int depth, double xFrequency, double zFrequency, double amplitude)
     {
-        int var14 = 0;
+        int counter = 0;
 
-        for (int var15 = 0; var15 < var6; ++var15)
+        for (int x1 = 0; x1 < width; ++x1)
         {
-            double var16 = (var2 + var15) * var8 + offsetX;
+            double x2 = (x + x1) * xFrequency + _xCoord;
 
-            for (int var18 = 0; var18 < var7; ++var18)
+            for (int z1 = 0; z1 < depth; ++z1)
             {
-                double var19 = (var4 + var18) * var10 + offsetY;
-                double var27 = (var16 + var19) * F2;
-                int var29 = wrap(var16 + var27);
-                int var30 = wrap(var19 + var27);
-                double var31 = (var29 + var30) * G2;
-                double var33 = var29 - var31;
-                double var35 = var30 - var31;
-                double var37 = var16 - var33;
-                double var39 = var19 - var35;
-                byte var41;
-                byte var42;
-                if (var37 > var39)
+                double z2 = (z + z1) * zFrequency + _yCoord;
+                double s = (x2 + z2) * F2;
+                int i = floor(x2 + s);
+                int j = floor(z2 + s);
+                double t = (i + j) * G2;
+                double x3 = i - t;
+                double z3 = j - t;
+                double x4 = x2 - x3;
+                double z4 = z2 - z3;
+                byte i1;
+                byte j1;
+                if (x4 > z4)
                 {
-                    var41 = 1;
-                    var42 = 0;
+                    i1 = 1;
+                    j1 = 0;
                 }
                 else
                 {
-                    var41 = 0;
-                    var42 = 1;
+                    i1 = 0;
+                    j1 = 1;
                 }
 
-                double var43 = var37 - var41 + G2;
-                double var45 = var39 - var42 + G2;
-                double var47 = var37 - 1.0D + 2.0D * G2;
-                double var49 = var39 - 1.0D + 2.0D * G2;
-                int var51 = var29 & 255;
-                int var52 = var30 & 255;
-                int var53 = perm[var51 + perm[var52]] % 12;
-                int var54 = perm[var51 + var41 + perm[var52 + var42]] % 12;
-                int var55 = perm[var51 + 1 + perm[var52 + 1]] % 12;
-                double var56 = 0.5D - var37 * var37 - var39 * var39;
-                double var21;
-                if (var56 < 0.0D)
+                double x5 = x4 - i1 + G2;
+                double z5 = z4 - j1 + G2;
+                double x6 = x4 - 1.0D + 2.0D * G2;
+                double z6 = z4 - 1.0D + 2.0D * G2;
+                int ii = i & 255;
+                int jj = j & 255;
+                int gi0 = _permutations[ii + _permutations[jj]] % 12;
+                int gi1 = _permutations[ii + i1 + _permutations[jj + j1]] % 12;
+                int gi2 = _permutations[ii + 1 + _permutations[jj + 1]] % 12;
+                double t0 = 0.5D - x4 * x4 - z4 * z4;
+                double n0;
+                if (t0 < 0.0D)
                 {
-                    var21 = 0.0D;
+                    n0 = 0.0D;
                 }
                 else
                 {
-                    var56 *= var56;
-                    var21 = var56 * var56 * func_4156_a(grads[var53], var37, var39);
+                    t0 *= t0;
+                    n0 = t0 * t0 * dot(grads[gi0], x4, z4);
                 }
 
-                double var58 = 0.5D - var43 * var43 - var45 * var45;
-                double var23;
-                if (var58 < 0.0D)
+                double t1 = 0.5D - x5 * x5 - z5 * z5;
+                double n1;
+                if (t1 < 0.0D)
                 {
-                    var23 = 0.0D;
+                    n1 = 0.0D;
                 }
                 else
                 {
-                    var58 *= var58;
-                    var23 = var58 * var58 * func_4156_a(grads[var54], var43, var45);
+                    t1 *= t1;
+                    n1 = t1 * t1 * dot(grads[gi1], x5, z5);
                 }
 
-                double var60 = 0.5D - var47 * var47 - var49 * var49;
-                double var25;
-                if (var60 < 0.0D)
+                double t2 = 0.5D - x6 * x6 - z6 * z6;
+                double n2;
+                if (t2 < 0.0D)
                 {
-                    var25 = 0.0D;
+                    n2 = 0.0D;
                 }
                 else
                 {
-                    var60 *= var60;
-                    var25 = var60 * var60 * func_4156_a(grads[var55], var47, var49);
+                    t2 *= t2;
+                    n2 = t2 * t2 * dot(grads[gi2], x6, z6);
                 }
 
-                int var10001 = var14++;
-                var1[var10001] += 70.0D * (var21 + var23 + var25) * var12;
+                buffer[counter++] += 70.0D * (n0 + n1 + n2) * amplitude;
             }
         }
-
     }
 }