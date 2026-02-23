239dc573040485300e39d98e127348765d82879a
tomast1337 <vycasnicolas@gmail.com>
2026-02-22 22:33:54 -0300

    Fix: Suppress CA5351 in MD5String for legacy checksums

--- BetaSharp/Util/MD5String.cs
+++ BetaSharp/Util/MD5String.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -7,13 +8,19 @@ public class MD5String
 {
     private readonly string salt;
 
-    public MD5String(string salt) => this.salt = salt;
+    public MD5String(string salt)
+    {
+        this.salt = salt;
+    }
 
+    // Suppress CA5351 because we need MD5 for legacy checksum compatibility, not security.
+#pragma warning disable CA5351 
     public string Hash(string str)
     {
-        string saltedString = salt + str;
-        byte[] inputBytes = Encoding.UTF8.GetBytes(saltedString);
-        byte[] hashBytes = MD5.HashData(inputBytes);
-        return Convert.ToHexString(hashBytes).ToLowerInvariant();
+        byte[] hashBytes = MD5.HashData(Encoding.UTF8.GetBytes(salt + str));
+        
+        // Replicates the legacy Java behavior where leading zeros are truncated
+        return new BigInteger(hashBytes, isUnsigned: true, isBigEndian: true).ToString("x");
     }
+#pragma warning restore CA5351
 }
