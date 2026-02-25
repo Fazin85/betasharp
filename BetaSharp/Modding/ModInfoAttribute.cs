7aab15fcf613dea078a54183ef8a49e207bdbf04
PurelyAndy <72871450+PurelyAndy@users.noreply.github.com>
2026-02-24 19:34:20 -0500

    WIP on feat/basic-mod-loading

--- BetaSharp/Modding/ModInfoAttribute.cs
+++ BetaSharp/Modding/ModInfoAttribute.cs
@@ -7,6 +7,16 @@ public class ModInfoAttribute : Attribute
     public readonly string Description;
     public readonly string Author;
 
+    /// <summary>
+    /// Creates a new ModInfoAttribute with the specified name, description, and author.
+    /// </summary>
+    /// <param name="name">The name of the mod.</param>
+    /// <param name="description">
+    /// A description of the mod, can be as long or short as you want.
+    /// The first line of the description will be shown in the mod list.
+    /// All lines will be shown in the mod's page when it is clicked on.
+    /// </param>
+    /// <param name="author">The author of the mod.</param>
     public ModInfoAttribute(string name, string description, string author)
     {
         Name = name;
