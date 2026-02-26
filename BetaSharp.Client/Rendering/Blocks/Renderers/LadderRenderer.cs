9f0d9296e1f6fe8bcd822b81e21ea0589c08e369
tomast1337 <vycasnicolas@gmail.com>
2026-02-26 12:20:55 -0300

    Refactor LadderRenderer

--- BetaSharp.Client/Rendering/Blocks/Renderers/LadderRenderer.cs
+++ BetaSharp.Client/Rendering/Blocks/Renderers/LadderRenderer.cs
@@ -7,18 +7,15 @@ namespace BetaSharp.Client.Rendering.Blocks.Renderers;
 
 public class LadderRenderer : IBlockRenderer
 {
-    public bool Render(IBlockAccess world, Block block, in BlockPos pos, Tessellator tess,
-        in BlockRenderContext context)
+    public bool Render(IBlockAccess world, Block block, in BlockPos pos, Tessellator tess, in BlockRenderContext context)
     {
-        Tessellator tess = _tess;
-
         int textureId = block.getTexture(0);
-        if (_overrideBlockTexture >= 0)
+        if (context.OverrideTexture >= 0)
         {
-            textureId = _overrideBlockTexture;
+            textureId = context.OverrideTexture;
         }
 
-        float luminance = block.getLuminance(_blockAccess, x, y, z);
+        float luminance = block.getLuminance(world, pos.x, pos.y, pos.z);
         tess.setColorOpaque_F(luminance, luminance, luminance);
 
         int texU = (textureId & 15) << 4;
@@ -28,36 +25,38 @@ public class LadderRenderer : IBlockRenderer
         double minV = texV / 256.0D;
         double maxV = (texV + 15.99D) / 256.0D;
 
-        int metadata = _blockAccess.getBlockMeta(x, y, z);
+        int metadata = world.getBlockMeta(pos.x, pos.y, pos.z);
+
+        // Push the ladder slightly off the wall to prevent Z-fighting
         double offset = 0.05D;
 
         if (metadata == 5)
         {
-            tess.addVertexWithUV(x + offset, y + 1.0D, z + 1.0D, minU, minV);
-            tess.addVertexWithUV(x + offset, y + 0.0D, z + 1.0D, minU, maxV);
-            tess.addVertexWithUV(x + offset, y + 0.0D, z + 0.0D, maxU, maxV);
-            tess.addVertexWithUV(x + offset, y + 1.0D, z + 0.0D, maxU, minV);
+            tess.addVertexWithUV(pos.x + offset, pos.y + 1.0D, pos.z + 1.0D, minU, minV);
+            tess.addVertexWithUV(pos.x + offset, pos.y + 0.0D, pos.z + 1.0D, minU, maxV);
+            tess.addVertexWithUV(pos.x + offset, pos.y + 0.0D, pos.z + 0.0D, maxU, maxV);
+            tess.addVertexWithUV(pos.x + offset, pos.y + 1.0D, pos.z + 0.0D, maxU, minV);
         }
         else if (metadata == 4)
         {
-            tess.addVertexWithUV(x + 1.0D - offset, y + 0.0D, z + 1.0D, maxU, maxV);
-            tess.addVertexWithUV(x + 1.0D - offset, y + 1.0D, z + 1.0D, maxU, minV);
-            tess.addVertexWithUV(x + 1.0D - offset, y + 1.0D, z + 0.0D, minU, minV);
-            tess.addVertexWithUV(x + 1.0D - offset, y + 0.0D, z + 0.0D, minU, maxV);
+            tess.addVertexWithUV(pos.x + 1.0D - offset, pos.y + 0.0D, pos.z + 1.0D, maxU, maxV);
+            tess.addVertexWithUV(pos.x + 1.0D - offset, pos.y + 1.0D, pos.z + 1.0D, maxU, minV);
+            tess.addVertexWithUV(pos.x + 1.0D - offset, pos.y + 1.0D, pos.z + 0.0D, minU, minV);
+            tess.addVertexWithUV(pos.x + 1.0D - offset, pos.y + 0.0D, pos.z + 0.0D, minU, maxV);
         }
         else if (metadata == 3)
         {
-            tess.addVertexWithUV(x + 1.0D, y + 0.0D, z + offset, maxU, maxV);
-            tess.addVertexWithUV(x + 1.0D, y + 1.0D, z + offset, maxU, minV);
-            tess.addVertexWithUV(x + 0.0D, y + 1.0D, z + offset, minU, minV);
-            tess.addVertexWithUV(x + 0.0D, y + 0.0D, z + offset, minU, maxV);
+            tess.addVertexWithUV(pos.x + 1.0D, pos.y + 0.0D, pos.z + offset, maxU, maxV);
+            tess.addVertexWithUV(pos.x + 1.0D, pos.y + 1.0D, pos.z + offset, maxU, minV);
+            tess.addVertexWithUV(pos.x + 0.0D, pos.y + 1.0D, pos.z + offset, minU, minV);
+            tess.addVertexWithUV(pos.x + 0.0D, pos.y + 0.0D, pos.z + offset, minU, maxV);
         }
         else if (metadata == 2)
         {
-            tess.addVertexWithUV(x + 1.0D, y + 1.0D, z + 1.0D - offset, minU, minV);
-            tess.addVertexWithUV(x + 1.0D, y + 0.0D, z + 1.0D - offset, minU, maxV);
-            tess.addVertexWithUV(x + 0.0D, y + 0.0D, z + 1.0D - offset, maxU, maxV);
-            tess.addVertexWithUV(x + 0.0D, y + 1.0D, z + 1.0D - offset, maxU, minV);
+            tess.addVertexWithUV(pos.x + 1.0D, pos.y + 1.0D, pos.z + 1.0D - offset, minU, minV);
+            tess.addVertexWithUV(pos.x + 1.0D, pos.y + 0.0D, pos.z + 1.0D - offset, minU, maxV);
+            tess.addVertexWithUV(pos.x + 0.0D, pos.y + 0.0D, pos.z + 1.0D - offset, maxU, maxV);
+            tess.addVertexWithUV(pos.x + 0.0D, pos.y + 1.0D, pos.z + 1.0D - offset, maxU, minV);
         }
 
         return true;
