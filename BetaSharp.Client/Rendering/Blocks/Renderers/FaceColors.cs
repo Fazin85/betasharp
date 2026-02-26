9a2f75e4b36d0441bf49f420951b93d44b8cf6bb
tomast1337 <vycasnicolas@gmail.com>
2026-02-26 11:49:33 -0300

    Add AssignVertexColors method to FaceColors

--- BetaSharp.Client/Rendering/Blocks/Renderers/FaceColors.cs
+++ BetaSharp.Client/Rendering/Blocks/Renderers/FaceColors.cs
@@ -18,4 +18,18 @@ public readonly ref struct FaceColors
         RedBottomRight = rBR; GreenBottomRight = gBR; BlueBottomRight = bBR;
         RedTopRight = rTR; GreenTopRight = gTR; BlueTopRight = bTR;
     }
+
+    public static FaceColors AssignVertexColors(float v0, float v1, float v2, float v3, float r, float g, float b, float faceShadow, bool tint)
+    {
+        float tr = (tint ? r : 1.0F) * faceShadow;
+        float tg = (tint ? g : 1.0F) * faceShadow;
+        float tb = (tint ? b : 1.0F) * faceShadow;
+
+        return new FaceColors(
+            tr * v0, tg * v0, tb * v0, // Top Left
+            tr * v1, tg * v1, tb * v1, // Bottom Left
+            tr * v2, tg * v2, tb * v2, // Bottom Right
+            tr * v3, tg * v3, tb * v3  // Top Right
+        );
+    }
 }
