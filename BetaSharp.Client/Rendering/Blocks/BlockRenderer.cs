cc8d163cc6b939df66f5c06002ac73c16c871b56
tomast1337 <vycasnicolas@gmail.com>
2026-02-26 15:54:50 -0300

    Read added RenderBlockOnInventory and RenderBlockFallingSand

--- BetaSharp.Client/Rendering/Blocks/BlockRenderer.cs
+++ BetaSharp.Client/Rendering/Blocks/BlockRenderer.cs
@@ -3,6 +3,8 @@ using BetaSharp.Client.Rendering.Blocks.Renderers;
 using BetaSharp.Client.Rendering.Core;
 using BetaSharp.Util.Maths;
 using BetaSharp.Worlds;
+using java.lang;
+using Math = System.Math;
 
 namespace BetaSharp.Client.Rendering.Blocks;
 
@@ -74,261 +76,137 @@ public class BlockRenderer
             _ => false
         };
     }
+
+
+    // TODO: THIS SHOULD BE PART OF THE UI
+    [Deprecated(["THIS SHOULD BE PART OF THE UI"])]
+    public void RenderBlockOnInventory(Block block, int metadata, float brightness, Tessellator tess)
+    {
+        int renderType = block.getRenderType();
+
+        int color = block.getColor(metadata);
+        float red = (color >> 16 & 255) / 255.0F * brightness;
+        float green = (color >> 8 & 255) / 255.0F * brightness;
+        float blue = (color & 255) / 255.0F * brightness;
+        GLManager.GL.Color4(red, green, blue, 1.0F);
+
+        var uiCtx = new BlockRenderContext(
+            world: null,
+            tess: tess,
+            renderAllFaces: true,
+            enableAo: false,
+            overrideTexture: (renderType == 16) ? 1 : -1
+        );
+
+        Vec3D origin = new Vec3D(0, 0, 0);
+        FaceColors dummyColors = new FaceColors();
+
+        if (IsSideLit(renderType))
+        {
+            block.setupRenderBoundingBox();
+            GLManager.GL.Translate(-0.5F, -0.5F, -0.5F);
+
+            // We manually draw the cube faces here to set GL Normals for the UI lighting
+            tess.startDrawingQuads();
+            tess.setNormal(0.0F, -1.0F, 0.0F);
+            uiCtx.DrawBottomFace(block, origin, dummyColors, block.getTexture(0, metadata));
+            tess.draw();
+
+            tess.startDrawingQuads();
+            tess.setNormal(0.0F, 1.0F, 0.0F);
+            uiCtx.DrawTopFace(block, origin, dummyColors, block.getTexture(1, metadata));
+            tess.draw();
+
+            tess.startDrawingQuads();
+            tess.setNormal(0.0F, 0.0F, -1.0F);
+            uiCtx.DrawEastFace(block, origin, dummyColors, block.getTexture(2, metadata));
+            tess.draw();
+
+            tess.startDrawingQuads();
+            tess.setNormal(0.0F, 0.0F, 1.0F);
+            uiCtx.DrawWestFace(block, origin, dummyColors, block.getTexture(3, metadata));
+            tess.draw();
+
+            tess.startDrawingQuads();
+            tess.setNormal(-1.0F, 0.0F, 0.0F);
+            uiCtx.DrawNorthFace(block, origin, dummyColors, block.getTexture(4, metadata));
+            tess.draw();
+
+            tess.startDrawingQuads();
+            tess.setNormal(1.0F, 0.0F, 0.0F);
+            uiCtx.DrawSouthFace(block, origin, dummyColors, block.getTexture(5, metadata));
+            tess.draw();
+
+            GLManager.GL.Translate(0.5F, 0.5F, 0.5F);
+        }
+        else
+        {
+            // For non-cube blocks (Torches, Flowers), we use their specialized renderers
+            BlockPos itemPos = new BlockPos(0, 0, 0);
+            RenderBlockByRenderType(null, block, itemPos, tess, uiCtx.OverrideTexture, true);
+        }
+    }
+
+    public void RenderBlockFallingSand(Block block, World world, int x, int y, int z, Tessellator tess)
+    {
+        // Directional shading multipliers for fake 3D depth
+        float lightBottom = 0.5F;
+        float lightTop = 1.0F;
+        float lightZ = 0.8F; // East/West faces
+        float lightX = 0.6F; // North/South faces
+
+        // Create a specialized Entity Context: No AO, Forced All Faces
+        var entityCtx = new BlockRenderContext(
+            world: world,
+            tess: tess,
+            renderAllFaces: true,
+            enableAo: false
+        );
+
+        tess.startDrawingQuads();
+
+        // Base luminance at the entity's current position
+        float currentLuminance = block.getLuminance(world, x, y, z);
+        Vec3D localOrigin = new Vec3D(-0.5, -0.5, -0.5);
+        FaceColors dummyColors = new FaceColors();
+
+        // --- Bottom Face ---
+        float faceLum = Math.Max(currentLuminance, block.getLuminance(world, x, y - 1, z));
+        tess.setColorOpaque_F(lightBottom * faceLum, lightBottom * faceLum, lightBottom * faceLum);
+        entityCtx.DrawBottomFace(block, localOrigin, dummyColors, block.getTexture(0));
+
+        // --- Top Face ---
+        faceLum = Math.Max(currentLuminance, block.getLuminance(world, x, y + 1, z));
+        tess.setColorOpaque_F(lightTop * faceLum, lightTop * faceLum, lightTop * faceLum);
+        entityCtx.DrawTopFace(block, localOrigin, dummyColors, block.getTexture(1));
+
+        // --- East/West Faces ---
+        faceLum = Math.Max(currentLuminance, block.getLuminance(world, x, y, z - 1));
+        tess.setColorOpaque_F(lightZ * faceLum, lightZ * faceLum, lightZ * faceLum);
+        entityCtx.DrawEastFace(block, localOrigin, dummyColors, block.getTexture(2));
+
+        faceLum = Math.Max(currentLuminance, block.getLuminance(world, x, y, z + 1));
+        tess.setColorOpaque_F(lightZ * faceLum, lightZ * faceLum, lightZ * faceLum);
+        entityCtx.DrawWestFace(block, localOrigin, dummyColors, block.getTexture(3));
+
+        // --- North/South Faces ---
+        faceLum = Math.Max(currentLuminance, block.getLuminance(world, x - 1, y, z));
+        tess.setColorOpaque_F(lightX * faceLum, lightX * faceLum, lightX * faceLum);
+        entityCtx.DrawNorthFace(block, localOrigin, dummyColors, block.getTexture(4));
+
+        faceLum = Math.Max(currentLuminance, block.getLuminance(world, x + 1, y, z));
+        tess.setColorOpaque_F(lightX * faceLum, lightX * faceLum, lightX * faceLum);
+        entityCtx.DrawSouthFace(block, localOrigin, dummyColors, block.getTexture(5));
+
+        tess.draw();
+    }
+
+    public static bool IsSideLit(int renderType)
+    {
+        return renderType == 0 || // Standard
+               renderType == 10 || // Stairs
+               renderType == 11 || // Fence
+               renderType == 13 || // Cactus
+               renderType == 16; // Piston Base
+    }
 }
-//public void RenderBlockFallingSand(Block block, World world, int x, int y, int z)
-//{
-//    // Directional shading multipliers for fake 3D depth
-//    float lightBottom = 0.5F;
-//    float lightTop = 1.0F;
-//    float lightZ = 0.8F; // East/West faces
-//    float lightX = 0.6F; // North/South faces
-//
-//    Tessellator tess = _tess;
-//    tess.startDrawingQuads();
-//
-//    // Base luminance at the entity's current position
-//    float currentLuminance = block.getLuminance(world, x, y, z);
-//
-//    // --- Bottom Face (Y - 1) ---
-//    float faceLuminance = block.getLuminance(world, x, y - 1, z);
-//    // Ensure the face isn't darker than the air block it occupies
-//    if (faceLuminance < currentLuminance) faceLuminance = currentLuminance;
-//
-//    tess.setColorOpaque_F(lightBottom * faceLuminance, lightBottom * faceLuminance, lightBottom * faceLuminance);
-//    // Note: Rendered at local origin (-0.5) because the entity's global transform handles the actual world position
-//    Helper.RenderBottomFace(block, -0.5D, -0.5D, -0.5D, block.getTexture(0));
-//
-//    // --- Top Face (Y + 1) ---
-//    faceLuminance = block.getLuminance(world, x, y + 1, z);
-//    if (faceLuminance < currentLuminance) faceLuminance = currentLuminance;
-//
-//    tess.setColorOpaque_F(lightTop * faceLuminance, lightTop * faceLuminance, lightTop * faceLuminance);
-//    Helper.RenderTopFace(block, -0.5D, -0.5D, -0.5D, block.getTexture(1));
-//
-//    // --- East Face (Z - 1) ---
-//    faceLuminance = block.getLuminance(world, x, y, z - 1);
-//    if (faceLuminance < currentLuminance) faceLuminance = currentLuminance;
-//
-//    tess.setColorOpaque_F(lightZ * faceLuminance, lightZ * faceLuminance, lightZ * faceLuminance);
-//    Helper.RenderEastFace(block, -0.5D, -0.5D, -0.5D, block.getTexture(2));
-//
-//    // --- West Face (Z + 1) ---
-//    faceLuminance = block.getLuminance(world, x, y, z + 1);
-//    if (faceLuminance < currentLuminance) faceLuminance = currentLuminance;
-//
-//    tess.setColorOpaque_F(lightZ * faceLuminance, lightZ * faceLuminance, lightZ * faceLuminance);
-//    Helper.RenderWestFace(block, -0.5D, -0.5D, -0.5D, block.getTexture(3));
-//
-//    // --- North Face (X - 1) ---
-//    faceLuminance = block.getLuminance(world, x - 1, y, z);
-//    if (faceLuminance < currentLuminance) faceLuminance = currentLuminance;
-//
-//    tess.setColorOpaque_F(lightX * faceLuminance, lightX * faceLuminance, lightX * faceLuminance);
-//    Helper.RenderNorthFace(block, -0.5D, -0.5D, -0.5D, block.getTexture(4));
-//
-//    // --- South Face (X + 1) ---
-//    faceLuminance = block.getLuminance(world, x + 1, y, z);
-//    if (faceLuminance < currentLuminance) faceLuminance = currentLuminance;
-//
-//    tess.setColorOpaque_F(lightX * faceLuminance, lightX * faceLuminance, lightX * faceLuminance);
-//    RenderSouthFace(block, -0.5D, -0.5D, -0.5D, block.getTexture(5));
-//
-//    tess.draw();
-//}
-
-//public void RenderBlockOnInventory(Block block, int metadata, float brightness)
-//{
-//    Tessellator tess = _tess;
-//    int renderType = block.getRenderType();
-//
-//    if (RenderFromInside)
-//    {
-//        int color = block.getColor(metadata);
-//        float red = (color >> 16 & 255) / 255.0F;
-//        float green = (color >> 8 & 255) / 255.0F;
-//        float blue = (color & 255) / 255.0F;
-//
-//        // Apply color and brightness to the global GL state
-//        GLManager.GL.Color4(red * brightness, green * brightness, blue * brightness, 1.0F);
-//    }
-//
-//    // Standard blocks (0) and Piston Bases (16) use standard 6-face cube rendering
-//    if (renderType == 0 || renderType == 16)
-//    {
-//        if (renderType == 16) metadata = 1; // Force standard texture for piston items
-//
-//        block.setupRenderBoundingBox();
-//        GLManager.GL.Translate(-0.5F, -0.5F, -0.5F);
-//
-//        tess.startDrawingQuads();
-//        tess.setNormal(0.0F, -1.0F, 0.0F);
-//        Helper.RenderBottomFace(block, 0.0D, 0.0D, 0.0D, block.getTexture(0, metadata));
-//        tess.draw();
-//
-//        tess.startDrawingQuads();
-//        tess.setNormal(0.0F, 1.0F, 0.0F);
-//        Helper.RenderTopFace(block, 0.0D, 0.0D, 0.0D, block.getTexture(1, metadata));
-//        tess.draw();
-//
-//        tess.startDrawingQuads();
-//        tess.setNormal(0.0F, 0.0F, -1.0F);
-//        Helper.RenderEastFace(block, 0.0D, 0.0D, 0.0D, block.getTexture(2, metadata));
-//        tess.draw();
-//
-//        tess.startDrawingQuads();
-//        tess.setNormal(0.0F, 0.0F, 1.0F);
-//        Helper.RenderWestFace(block, 0.0D, 0.0D, 0.0D, block.getTexture(3, metadata));
-//        tess.draw();
-//
-//        tess.startDrawingQuads();
-//        tess.setNormal(-1.0F, 0.0F, 0.0F);
-//        Helper.RenderNorthFace(block, 0.0D, 0.0D, 0.0D, block.getTexture(4, metadata));
-//        tess.draw();
-//
-//        tess.startDrawingQuads();
-//        tess.setNormal(1.0F, 0.0F, 0.0F);
-//        RenderSouthFace(block, 0.0D, 0.0D, 0.0D, block.getTexture(5, metadata));
-//        tess.draw();
-//
-//        GLManager.GL.Translate(0.5F, 0.5F, 0.5F);
-//    }
-//    else if (renderType == 1) // Crossed squares (Flowers, Saplings)
-//    {
-//        tess.startDrawingQuads();
-//        tess.setNormal(0.0F, -1.0F, 0.0F);
-//        RenderCrossedSquares(block, metadata, -0.5D, -0.5D, -0.5D);
-//        tess.draw();
-//    }
-//    else if (renderType == 13) // Cactus (slightly inset faces)
-//    {
-//        block.setupRenderBoundingBox();
-//        GLManager.GL.Translate(-0.5F, -0.5F, -0.5F);
-//        float inset = 1.0F / 16.0F;
-//
-//        tess.startDrawingQuads();
-//        tess.setNormal(0.0F, -1.0F, 0.0F);
-//        Helper.RenderBottomFace(block, 0.0D, 0.0D, 0.0D, block.getTexture(0));
-//        tess.draw();
-//
-//        tess.startDrawingQuads();
-//        tess.setNormal(0.0F, 1.0F, 0.0F);
-//        Helper.RenderTopFace(block, 0.0D, 0.0D, 0.0D, block.getTexture(1));
-//        tess.draw();
-//
-//        tess.startDrawingQuads();
-//        tess.setNormal(0.0F, 0.0F, -1.0F);
-//        tess.setTranslationF(0.0F, 0.0F, inset);
-//        Helper.RenderEastFace(block, 0.0D, 0.0D, 0.0D, block.getTexture(2));
-//        tess.setTranslationF(0.0F, 0.0F, -inset);
-//        tess.draw();
-//
-//        tess.startDrawingQuads();
-//        tess.setNormal(0.0F, 0.0F, 1.0F);
-//        tess.setTranslationF(0.0F, 0.0F, -inset);
-//        Helper.RenderWestFace(block, 0.0D, 0.0D, 0.0D, block.getTexture(3));
-//        tess.setTranslationF(0.0F, 0.0F, inset);
-//        tess.draw();
-//
-//        tess.startDrawingQuads();
-//        tess.setNormal(-1.0F, 0.0F, 0.0F);
-//        tess.setTranslationF(inset, 0.0F, 0.0F);
-//        Helper.RenderNorthFace(block, 0.0D, 0.0D, 0.0D, block.getTexture(4));
-//        tess.setTranslationF(-inset, 0.0F, 0.0F);
-//        tess.draw();
-//
-//        tess.startDrawingQuads();
-//        tess.setNormal(1.0F, 0.0F, 0.0F);
-//        tess.setTranslationF(-inset, 0.0F, 0.0F);
-//        RenderSouthFace(block, 0.0D, 0.0D, 0.0D, block.getTexture(5));
-//        tess.setTranslationF(inset, 0.0F, 0.0F);
-//        tess.draw();
-//
-//        GLManager.GL.Translate(0.5F, 0.5F, 0.5F);
-//    }
-//    else if (renderType == 6) // Crops (Wheat/Seeds)
-//    {
-//        tess.startDrawingQuads();
-//        tess.setNormal(0.0F, -1.0F, 0.0F);
-//        RenderCropQuads(block, metadata, -0.5D, -0.5D, -0.5D);
-//        tess.draw();
-//    }
-//    else if (renderType == 2) // Torch
-//    {
-//        tess.startDrawingQuads();
-//        tess.setNormal(0.0F, -1.0F, 0.0F);
-//        RenderTorchAtAngle(block, -0.5D, -0.5D, -0.5D, 0.0D, 0.0D);
-//        tess.draw();
-//    }
-//    else if (renderType == 10) // Stairs
-//    {
-//        for (int i = 0; i < 2; ++i)
-//        {
-//            if (i == 0) block.setBoundingBox(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 0.5F);
-//            if (i == 1) block.setBoundingBox(0.0F, 0.0F, 0.5F, 1.0F, 0.5F, 1.0F);
-//
-//            GLManager.GL.Translate(-0.5F, -0.5F, -0.5F);
-//            RenderCubeItem(block, tess);
-//            GLManager.GL.Translate(0.5F, 0.5F, 0.5F);
-//        }
-//    }
-//    else if (renderType == 11) // Fence
-//    {
-//        for (int i = 0; i < 4; ++i)
-//        {
-//            float size = 2.0F / 16.0F;
-//            if (i == 0) block.setBoundingBox(0.5F - size, 0.0F, 0.0F, 0.5F + size, 1.0F, size * 2.0F);
-//            if (i == 1) block.setBoundingBox(0.5F - size, 0.0F, 1.0F - size * 2.0F, 0.5F + size, 1.0F, 1.0F);
-//
-//            size = 1.0F / 16.0F;
-//            if (i == 2)
-//                block.setBoundingBox(0.5F - size, 1.0F - size * 3.0F, -size * 2.0F, 0.5F + size, 1.0F - size,
-//                    1.0F + size * 2.0F);
-//            if (i == 3)
-//                block.setBoundingBox(0.5F - size, 0.5F - size * 3.0F, -size * 2.0F, 0.5F + size, 0.5F - size,
-//                    1.0F + size * 2.0F);
-//
-//            GLManager.GL.Translate(-0.5F, -0.5F, -0.5F);
-//            RenderCubeItem(block, tess);
-//            GLManager.GL.Translate(0.5F, 0.5F, 0.5F);
-//        }
-//
-//        block.setBoundingBox(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
-//    }
-//}
-
-//private void RenderCubeItem(Block block, Tessellator tess)
-//{
-//    tess.startDrawingQuads();
-//    tess.setNormal(0.0F, -1.0F, 0.0F);
-//    Helper.RenderBottomFace(block, 0.0D, 0.0D, 0.0D, block.getTexture(0));
-//    tess.draw();
-//    tess.startDrawingQuads();
-//    tess.setNormal(0.0F, 1.0F, 0.0F);
-//    Helper.RenderTopFace(block, 0.0D, 0.0D, 0.0D, block.getTexture(1));
-//    tess.draw();
-//    tess.startDrawingQuads();
-//    tess.setNormal(0.0F, 0.0F, -1.0F);
-//    Helper.RenderEastFace(block, 0.0D, 0.0D, 0.0D, block.getTexture(2));
-//    tess.draw();
-//    tess.startDrawingQuads();
-//    tess.setNormal(0.0F, 0.0F, 1.0F);
-//    Helper.RenderWestFace(block, 0.0D, 0.0D, 0.0D, block.getTexture(3));
-//    tess.draw();
-//    tess.startDrawingQuads();
-//    tess.setNormal(-1.0F, 0.0F, 0.0F);
-//    Helper.RenderNorthFace(block, 0.0D, 0.0D, 0.0D, block.getTexture(4));
-//    tess.draw();
-//    tess.startDrawingQuads();
-//    tess.setNormal(1.0F, 0.0F, 0.0F);
-//    RenderSouthFace(block, 0.0D, 0.0D, 0.0D, block.getTexture(5));
-//    tess.draw();
-//}
-
-//public static bool IsSideLit(int renderType)
-//{
-//    return renderType == 0 || // Standard
-//           renderType == 10 || // Stairs
-//           renderType == 11 || // Fence
-//           renderType == 13 || // Cactus
-//           renderType == 16; // Piston Base
-//}
