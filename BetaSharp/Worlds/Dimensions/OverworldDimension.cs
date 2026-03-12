fffa2a0c1876a785f84af9305ac12e4ac7c5d0b7
tomast1337 <vycasnicolas@gmail.com>
2026-03-12 13:08:36 -0300

    Enhance OverworldDimension: Implement chunk generator and spawn point validation based on terrain type

--- BetaSharp/Worlds/Dimensions/OverworldDimension.cs
+++ BetaSharp/Worlds/Dimensions/OverworldDimension.cs
@@ -1,5 +1,37 @@
+using BetaSharp.Worlds;
+using BetaSharp.Worlds.Chunks;
+using BetaSharp.Worlds.Gen.Chunks;
+using BetaSharp.Worlds.Gen.Flat;
+using BetaSharp.Worlds.Generation.Generators.Chunks;
+
 namespace BetaSharp.Worlds.Dimensions;
 
 internal class OverworldDimension : Dimension
 {
+    public override IChunkSource CreateChunkGenerator()
+    {
+        WorldType terrainType = World.getProperties().TerrainType;
+
+        if (terrainType == WorldType.Flat)
+        {
+            return new FlatIChunkGenerator(World);
+        }
+
+        if (terrainType == WorldType.Sky)
+        {
+            return new SkyIChunkGenerator(World, World.getSeed());
+        }
+
+        return base.CreateChunkGenerator();
+    }
+
+    public override bool IsValidSpawnPoint(int x, int z)
+    {
+        if (World.getProperties().TerrainType == WorldType.Flat)
+        {
+            return true;
+        }
+
+        return base.IsValidSpawnPoint(x, z);
+    }
 }
